73076abc830877b8d5e032282bb4ad7058825554
zhou <634534398@qq.com>
2016-08-11 21:55:54 +0800

    init初始化！

---
+++ app/src/main/java/com/m_diary/wechatlogindemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.m_diary.wechatlogindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
